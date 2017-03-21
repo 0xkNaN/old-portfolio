a4ed2f776da5fa0f03d0648138a4dda44c418123
Hassen Rmili <hassen.rmili.hr4@gmail.com>
2017-03-22 00:43:05 +0100

    update add to char

--- works/addToCart/assets/scripts/main-js.js
+++ works/addToCart/assets/scripts/main-js.js
@@ -1,9 +1,30 @@
 
 
-var chartBtn = document.getElementsByClassName("add-cart-btn")[0];
+var chartBtns = document.getElementsByClassName("item-add-btn");
 var chart = document.getElementsByClassName("add-cart")[0];
 var chartItem = document.getElementsByClassName("add-cart-item-num")[0];
 var chartState = "hidden";
+
+
+
+//console.log(chartBtns);
+
+for(var btn in chartBtns){
+	chartBtns[btn].onclick = function() {
+		var  b = this;
+		b.classList.add("clicked");
+		setTimeout(function() {
+			b.classList.remove("clicked");
+			chartItem.innerText = +chartItem.innerText +1;
+			chart.classList.add("up");
+		}, 100);
+	}
+}
+
+
+
+
+/*
 
 chartBtn.onclick = function() {
 	chartBtn.classList.add("clicked");
@@ -11,7 +32,7 @@ chartBtn.onclick = function() {
 		chartBtn.classList.remove("clicked");
 	},150);
 	
-	/* Cart Animation */
+	// Cart Animation 
 	chartItem.innerText = +chartItem.innerText +1;
 	chart.classList.add("up");
-}
+}*/
